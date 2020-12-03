100f51b75582d8c22ccf4fe1a063c94699df46ee
Mila <kaah.fernandess@hotmail.com>
2020-12-03 18:46:59 -0300

    Aula 034 até aqui

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,26 @@
+const handle = 'milafrn'
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/"
+  },
+  {
+    label: "Youtube",
+    url: "https://youtube.com/"
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/"
+  },
+  {
+    label: "Unsplash",
+    url: "https://unsplash.com/"
+  }
+]
+
+export default links
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem
+      slug="/about/"
+      category="Misc"
+      date="30 de Julho de 2019"
+      timeToRead="5"
+      title="diga não ao Medium: tenha sua própria plataforma"
+      description="algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium"
+    />
   </Layout>
 )
 
